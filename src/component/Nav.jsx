04111c5045d5c1cade776b772b6eb5847ffc5c62
zeba <zebakhan0316@gmail.com>
2024-08-16 16:44:20 +0530

    sticky made

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Nav = ({scrollToOverview, scrollToAuth, scrollToTutorial}) => {
   return (
-    <div className=" w-full min-h-[10vh] flex flex-col items-center justify-center bg-[#1e293b] gap-8 py-5 md:flex-row ">
+    <div className=" w-full min-h-[10vh] flex flex-col items-center justify-center bg-[#1e293b] gap-8 py-5 md:flex-row sticky top-0">
       <p className="text-white text-xl font-medium hover:text-[#22d3ee] transition-colors duration-250 cursor-pointer" onClick={scrollToOverview}>OVERVIEW</p>
       <p className="text-white text-xl font-medium hover:text-[#22d3ee] transition-colors duration-250 cursor-pointer" onClick={scrollToAuth}>AUTHENTICATION</p>
       <p className="text-white text-xl font-medium hover:text-[#22d3ee] transition-colors duration-250 cursor-pointer">ENDPOINTS</p>
